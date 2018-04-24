49c35ae6f091a8d59ec8f14747c9b1bb8e6c7fd9
Abhijeet Kandalkar <kandalkar.abhijeet58@gmail.com>
2018-04-24 19:02:55 +0530

    Intuit F2F round question

---
+++ Intuit-F2F-Round-Question.cpp
@@ -0,0 +1,24 @@
+/*
+Given a file and input of 'n' words, return back :
+A] If all 'n' words exist.
+B] If A] is TRUE, are there multiple occurrences of ATLEAST one of these words.
+C] If B] is TRUE, return the positions (line and word number) of these
+words where they are space closest to each other.
+
+Sample Input :
+----------------------------------------------------------------------------------------
+File contents are as below :
+>>>
+Sachin has the highest number of Test centuries.
+Sachin has the highest number of ODI centuries.
+Sachin has total 100 centuries in which 51 are test centuries and 49 are ODI centuries.
+>>>
+
+The words to be search 'Sachin' and 'centuries'
+
+Your answer should look like below :
+A] TRUE as both words exist in content
+B] TRUE, word 'Sachin' has 3 occurrences and word 'centuries' has 5 occurrences
+C] Line number 3 and word number 1 and 5.
+*/
+
