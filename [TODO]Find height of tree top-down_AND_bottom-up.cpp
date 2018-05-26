1b2e142ecd906a1abb9c756de66661739a19cba2
Abhijeet Kandalkar <kandalkar.abhijeet58@gmail.com>
2018-05-26 17:48:38 +0530

    Height of tree using top down and bottom up approach

---
+++ [TODO]Find height of tree top-down_AND_bottom-up.cpp
@@ -0,0 +1,9 @@
+/*
+Bottom up approach :
+Consider height of root as '0' and then successively find height of node at level 1 and so on.
+Similar to dynamic programming approach
+
+Top down approach :
+Consider height of leaf as '0' and then successively find height of parent node.
+*/
+
